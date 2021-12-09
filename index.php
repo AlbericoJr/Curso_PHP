58183dab023ea256f786c8502970e3a66f376fa2
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2021-12-09 18:58:33 -0300

    Desafio Equação

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
                     <h3>variaveis</h3>
                     <ul>
                         <li><a href="exercicios.php?dir=variaveis&file=basico">variaveis</a></li>
+                        <li><a href="exercicios.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
                     </ul>
                 </div>
             </nav>
