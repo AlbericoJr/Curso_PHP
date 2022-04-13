7d737a574a013df5e64c9161611051f2445ab8c1
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-04-13 20:02:09 -0300

    if/Else #02

--- controle/if_else.php
+++ controle/if_else.php
@@ -4,5 +4,30 @@
 
 if(true){
     echo "Serei impresso? <br>";
-    echo "Serei impresso novamente?";
+    echo "Serei impresso novamente?<br>";
 }
+
+if(false){
+    echo"Verdadeiro - Parte A<br>";
+    echo"Verdadeiro - Parte B<br>";
+}else{
+    echo"Falso - Parte A<br>";
+    echo"Falso - Parte B<br>";
+}
+
+if(false){
+    echo"Passo A <br>";
+}else if(false){
+    echo"Passo B<br>";
+}elseif(false){
+    echo"Passo C<br>";
+}else if(false){
+    echo"Passo D<br>";
+}else if(true){
+    echo"Passo E<br>";
+}else{
+    echo"último Passo<br>";
+}
+
+
+echo"Fim<br>";
