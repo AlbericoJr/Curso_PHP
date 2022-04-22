06b1c1097fb04b1d57618461727269dce8ead931
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-04-22 18:36:01 -0300

    Desafio Switch #01

--- controle/desafio_switch01.php
+++ controle/desafio_switch01.php
@@ -18,5 +18,30 @@
 </style>
 
 <?php
-echo $_POST['param'] ; '<br>';
-echo_$_POST['conversao']
+
+const FATOR_KM_MILHA = 0.621371;
+const FATOR_METRO_KM = 1000;
+
+$param = $_POST['param'] ?? '0';
+switch ($_POST['conversao']){
+    case'km-milha':
+        $distancia = $param * FATOR_KM_MILHA;
+        $mensagem = "$param km(S) = $distancia Milha(S)";
+        break;
+    case 'milha-km':
+        $distancia = $param / FATOR_KM_MILHA;
+        $mensagem = "$param Milha(S) = $distancia km(S)";
+        break;
+    case 'metro-km':
+        $distancia = $param / FATOR_METRO_KM;
+        $mensagm = "$oaram Metro(s) = $distancia km(s)";
+        break;
+    case 'km-metro':
+        $distancia = $param * FATOR_METRO_KM;
+        $mensagem = "$param km(S) = $distancia Metro(S)";
+        break;
+    default:
+        $mensagem = "Nenhum valor calculado!";
+}
+
+echo "<p>$mensagem</p>";
