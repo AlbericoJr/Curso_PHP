14e4da7b4dfc8499a494e4753acb4c631a2a7670
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-05-02 18:15:22 -0300

    Break / Continue

--- index.php
+++ index.php
@@ -89,6 +89,7 @@
                         <li><a href="exercicios.php?dir=repeticoes&file=for">Laço For</a></li>
                         <li><a href="exercicios.php?dir=repeticoes&file=desafio_for">Desafio Laço For</a></li>
                         <li><a href="exercicios.php?dir=repeticoes&file=foreach">Foreach</a></li>
+                        <li><a href="exercicios.php?dir=repeticoes&file=break-continue">Break / Continue</a></li>
                     </ul>
                 </div>
             </nav>
