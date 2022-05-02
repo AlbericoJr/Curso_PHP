3d87d9378d70664e7d3804feffb83cb3e540b71b
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-05-02 18:40:25 -0300

    While / Do While

--- index.php
+++ index.php
@@ -91,6 +91,7 @@
                         <li><a href="exercicios.php?dir=repeticoes&file=foreach">Foreach</a></li>
                         <li><a href="exercicios.php?dir=repeticoes&file=break-continue">Break / Continue</a></li>
                         <li><a href="exercicios.php?dir=repeticoes&file=impressao">Desafio Impressão</a></li>
+                        <li><a href="exercicios.php?dir=repeticoes&file=while">While / Do while</a></li>
                     </ul>
                 </div>
             </nav>
