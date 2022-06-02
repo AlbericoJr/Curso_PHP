8ddf146eeb6ee69d21a8a6a445b4e12a1d06bbeb
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-06-02 19:52:27 -0300

    Include Once/ Require Once

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
                         <li><a href="exercicios.php?dir=includes&file=include">Include</a></li>
                         <li><a href="exercicios.php?dir=includes&file=include_funcao">Include em Função</a></li>
                         <li><a href="exercicios.php?dir=includes&file=include_require">Include vs Require</a></li>
-                        <li><a href="exercicios.php?dir=includes&file=require_return">Require & Return</a></li>
+                        <li><a href="exercicios.php?dir=includes&file=include_once">Include Once</a></li>
                         
                     </ul>
                 </div>
