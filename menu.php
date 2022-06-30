df9f4f3b7e738049cb785dd9ba6b79fcd0903fd4
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-06-30 19:24:09 -0300

    Upload de Arquivos

--- menu.php
+++ menu.php
@@ -6,6 +6,7 @@
             <li><a href="exercicio.php?dir=api&file=datas_02">Datas #02</a></li>
             <li><a href="exercicio.php?dir=api&file=escrever_arquivo">Escrevendo Arquivos</a></li>
             <li><a href="exercicio.php?dir=api&file=ler_arquivo">Lendo Arquivos</a></li>
+            <li><a href="exercicio.php?dir=api&file=upload">Upload de Arquivos</a></li>
         </ul>
     </div>
     <div class="modulo vermelho">
