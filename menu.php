a1ebb2ba18ec05ddecae1423da2f27d68b694b94
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-07-01 18:00:34 -0300

    Criando o Formulário

--- menu.php
+++ menu.php
@@ -1,4 +1,10 @@
 <nav class="modulos">
+    <div class="modulo roxo">
+        <h3>14. Formulário</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=formulario&file=formulario">Criando o Formulário</a></li>
+        </ul>
+    </div>
     <div class="modulo azul">
         <h3>13. API</h3>
         <ul>
